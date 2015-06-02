e301ad89a32b56193deec5ce31c088bd9ae8d47f
cnkaptan <l5208019@std.yildiz.edu.tr>
2015-06-02 10:53:29 +0200

    init project

---
+++ app/src/main/java/com/cihankaptan/android/linkedinoauthsample/LinkedIn.java
@@ -0,0 +1,46 @@
+package com.cihankaptan.android.linkedinoauthsample;
+
+/**
+ * Created by cnkaptan on 6/2/15.
+ */
+public class LinkedIn {
+
+    /*CONSTANT FOR THE AUTHORIZATION PROCESS*/
+
+    /****FILL THIS WITH YOUR INFORMATION*********/
+    private static final String API_KEY = "78r6zrlx2dosv7";
+    private static final String SECRET_KEY = "5AESBi80cPlibAif";
+    private static final String STATE = "DCEEFWF45453sdffef424";
+    private static final String REDIRECT_URI = "http://conichi.com";
+    /*********************************************/
+
+//These are constants used for build the urls
+    private static final String AUTHORIZATION_URL = "https://www.linkedin.com/uas/oauth2/authorization";
+    private static final String ACCESS_TOKEN_URL = "https://www.linkedin.com/uas/oauth2/accessToken";
+    private static final String SECRET_KEY_PARAM = "client_secret";
+    private static final String RESPONSE_TYPE_PARAM = "response_type";
+    private static final String GRANT_TYPE_PARAM = "grant_type";
+    private static final String GRANT_TYPE = "authorization_code";
+    private static final String RESPONSE_TYPE_VALUE ="code";
+    private static final String CLIENT_ID_PARAM = "client_id";
+    private static final String STATE_PARAM = "state";
+    private static final String REDIRECT_URI_PARAM = "redirect_uri";
+    /*---------------------------------------*/
+    private static final String QUESTION_MARK = "?";
+    private static final String AMPERSAND = "&";
+    private static final String EQUALS = "=";
+
+    private static String getAccessTokenUrl(String authorizationToken){
+        return ACCESS_TOKEN_URL
+                +QUESTION_MARK
+                +GRANT_TYPE_PARAM+EQUALS+GRANT_TYPE
+                +AMPERSAND
+                +RESPONSE_TYPE_VALUE+EQUALS+authorizationToken
+                +AMPERSAND
+                +CLIENT_ID_PARAM+EQUALS+API_KEY
+                +AMPERSAND
+                +REDIRECT_URI_PARAM+EQUALS+REDIRECT_URI
+                +AMPERSAND
+                +SECRET_KEY_PARAM+EQUALS+SECRET_KEY;
+    }
+}
